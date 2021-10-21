7267aba1b388ff8503d7b08b556b2cf7eeb8ac57
Adriel Modollo <adrielmodollo@outlook.com>
2021-10-21 20:03:29 -0300

    Abstract

--- Aula06/src/aula06/ControleRemoto.java
+++ Aula06/src/aula06/ControleRemoto.java
@@ -1,6 +1,6 @@
 package aula06;
 
-public class ControleRemoto {
+public class ControleRemoto implements Controlador {
 	private int volume;
 	private boolean ligado;
 	private boolean tocando;
@@ -34,6 +34,67 @@ public class ControleRemoto {
 
 	public void setTocando(boolean tocando) {
 		this.tocando = tocando;
+	}
+
+	//Metodos abstratos
+	@Override //Override significa descrever
+	public void ligar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void desligar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void abrirMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fecharMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void maisVolume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void menosVolume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ligarMudo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void desligarMudo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	
