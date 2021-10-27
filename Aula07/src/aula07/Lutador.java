09f77f4ec78989a513d6fb862cbdbe57e0828e51
Adriel Modollo <adrielmodollo@outlook.com>
2021-10-26 23:40:02 -0300

    Métodos construtor

--- Aula07/src/aula07/Lutador.java
+++ Aula07/src/aula07/Lutador.java
@@ -1,5 +1,35 @@
 package aula07;
 
 public class Lutador {
-
+	//Atributos
+	private String nome;
+	private String nacionalidade;
+	private int idade;
+	private float peso;
+	private String categoria;
+	private int vitorias, derrotas, empates; 
+	
+	//Métodos públicos
+	
+	public void apresentar() {
+		
+	}
+	
+	public void status() {
+		
+	}
+	
+	public void ganharLuta() {
+		
+	}
+	
+	public void perderLuta() {
+		
+	}
+	
+	public void empatarLuta() {
+		
+	}
+	
+	//Métodos especiais
 }
